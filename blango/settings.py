86137978078ea31591749fb4ea2f088adbb86b04
Mohammad Haque <mohammad.haque@itcinfotech.com>
2025-06-04 10:39:18 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -48,12 +48,17 @@ class Dev(Configuration):
         'django.contrib.contenttypes',
         'django.contrib.sessions',
         'django.contrib.messages',
+        "django.contrib.sites",
         'django.contrib.staticfiles',
         'blog',
         'crispy_forms',
         'crispy_bootstrap5',
         "debug_toolbar",
         "blango_auth",
+        "allauth",
+        "allauth.account",
+        "allauth.socialaccount",
+        "allauth.socialaccount.providers.google",
     ]
 
     MIDDLEWARE = [
@@ -167,6 +172,13 @@ class Dev(Configuration):
     ACCOUNT_ACTIVATION_DAYS = 7
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
     # REGISTRATION_OPEN = False
+    SITE_ID = 1
+
+    #  we need to define as we have changed the username to Email
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
 
     LOGGING = {
         "version": 1,
